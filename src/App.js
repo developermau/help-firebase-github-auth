cd1a122c9d40c0125e26b6000faf2ab6dacde658
Alejandro <mauriciorojas.auxiliar@outlook.com>
2021-06-20 02:16:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import { useState } from "react";
 import { auth } from "./lib/firebase";
 import "./styles.css";
 
 export default function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState("");
+
   const handlerAuthGithub = () => {
-    // console.log("Auth");
-    // console.log({ auth });
-    // console.log(fb.auth);
     console.clear();
     var provider = new firebase.auth.GithubAuthProvider();
     auth
       .signInWithPopup(provider)
       .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
         var credential = result.credential;
-        console.log({ credential });
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
         var token = credential.accessToken;
-        console.log({ token });
-        // The signed-in user info.
         var user = result.user;
-        // ...
-        console.log({ user });
+        setToken(token);
+        setUser(user);
+        setIsAuth(true);
       })
       .catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-        console.log({ errorCode, errorMessage, email, credential });
+        console.error(error);
       });
   };
   return (
     <div className="App">
-      <button onClick={handlerAuthGithub}>Auth Github</button>
+      {!isAuth && <button onClick={handlerAuthGithub}>Auth Github</button>}
+      {isAuth && token && (
+        <div>
+          <pre>{JSON.stringify(user)}</pre>
+        </div>
+      )}
     </div>
   );
 }
